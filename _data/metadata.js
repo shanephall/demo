7fb53780cf839dcc775b1a3c995d632097bea2a2
Shane Hall <shane@damntough.com>
2025-01-24 12:26:56 -0800

    Update _data/metadata.js (via Pages CMS)

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,5 +1,5 @@
 module.exports = {
-	title: "HillsdaleCommunityFoundation.ORG",
+	title: "The Hillsdale Community Foundation",
 	url: "https://www.hillsdalecommunityfoundation.org",
 	language: "en",
 	description: "The Hillsdale Community Foundation exists to strengthen community among those who live and work in Hillsdale.",
